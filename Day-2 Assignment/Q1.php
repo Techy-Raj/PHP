b705b0890c77684e526fc801eb047e4529942598
Techy-Raj <rajmanodra10the2@gmail.com>
2025-01-18 12:53:14 +0000

    Add files via upload

---
+++ Day-2 Assignment/Q1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Q1</title>
+</head>
+<body>
+    <?php
+        $sum = 0;
+        for($i=0; $i<=100; $i++){
+            if($i%2==0){
+                $sum +=$i;
+            }   
+        }
+        echo "The Sum of all Even Numbers between 1 and 100 is: ".$sum;
+    ?>
+    <style>
+        body{
+            text-align:center;
+            font-weight:bold;
+            font-size:30px;
+        }
+    </style>
+</body>
+</html>
---
+++ Day-2 Assignment/Q2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Q2</title>
+</head>
+<body>
+    <?php
+        $cities =array(
+            "Delhi"=>7054678,
+            "Mumbai"=>9054862,
+            "Agra"=>6054651,
+        );
+        $max_pop=max($cities);
+        $max_name = array_search($max_pop,$cities);
+        echo "City with highest population is $max_name with population of $max_pop";
+    ?>
+    <style>
+        body{
+            text-align:center;
+            font-weight:bold;
+            font-size:30px;
+        }
+    </style>
+</body>
+</html>
